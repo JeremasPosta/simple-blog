38111eb58f4e3e2efacb60e65a68de09d6fcb039
Jeremias Ramirez <60799999+JeremasPosta@users.noreply.github.com>
2021-11-16 23:58:37 -0300

    feat(User): User model and tests created and working.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  has_secure_password
+  before_validation :downcase_email
+
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+
+  validates :password, presence: true, length: { minimum: 6 }
+
+  private
+  def downcase_email
+    email.downcase! if email
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject{User.create(email: "example@example.com", password:"123456789")}
+
+  context "at creation" do
+
+    it "should save correctly with correct params" do
+      subject.email = "example2@example2.com"
+      subject.password = "123456"
+      expect(subject).to be_valid
+    end
+
+    it "should have an email" do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "email should be unique" do
+      subject.email = User.last.email
+      expect(subject).to_not be_valid
+    end
+    
+    it "email should be maximum 255 chars" do
+      subject.email = "a" * 255  + "@lipsum.com"
+      expect(subject).to_not be_valid
+    end
+
+    it "email should have email-format" do
+      subject.email = "example"
+      expect(subject).to_not be_valid
+    end
+
+    it "should have a password" do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "password should be at least 6 character length" do
+      subject.password = "123"
+      expect(subject).to_not be_valid
+    end
+
+  end
+
 end
