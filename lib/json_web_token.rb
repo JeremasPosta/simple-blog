94a108a888f6ccebd841f7531afe171637d87cce
Jeremias Ramirez <60799999+JeremasPosta@users.noreply.github.com>
2021-11-18 00:33:26 -0300

    fix(JWT): Mock token moved from file.

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,13 +1,13 @@
 class JsonWebToken
   class << self
-    SALTY = '07987fe0912905527d899213d65029b6aafb26f2d271fd23ba26eb0ddf7be278e0aa7df2fef6eac2a8c633b32633beb53923490b2e40ff21a168ea1a461a1e14'
+    
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, SALTY)
+      JWT.encode(payload, ENV['SALTY'])
     end
  
     def decode(token)
-      body = JWT.decode(token, SALTY)[0]
+      body = JWT.decode(token, ENV['SALTY'])[0]
       HashWithIndifferentAccess.new body
     rescue
       nil
