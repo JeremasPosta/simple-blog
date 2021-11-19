b279d1a16d1cc2f1230cb0090523427eee978fd4
Jeremias Ramirez <60799999+JeremasPosta@users.noreply.github.com>
2021-11-19 19:12:49 -0300

    tests(Post): Some initial tests added.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject{
+    Post.create(
+                title: "Post",
+                content: "Contenido del post.",
+                image: "image.jpg",
+                category: Category.create(name: "Tech"),
+                user_id: User.first.id
+              )
+  }
+
+  context "at creation" do
+    
+    it "should save with correct params." do
+      expect(subject).to be_valid
+    end
+
+    it "should have a title" do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "should have a content" do
+      subject.content = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "should have an image" do
+      subject.image = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "should have a User_id associated" do
+      subject.user_id = nil
+      expect(subject).to_not be_valid
+    end
+
+  end
+
 end
