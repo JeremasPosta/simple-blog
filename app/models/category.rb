4951c2419d60140eb18ed5442b9bcc6a75601d8a
Jeremias Ramirez <60799999+JeremasPosta@users.noreply.github.com>
2021-11-19 19:16:24 -0300

    feat(models): Validations added for three models (Post, Category, and user.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  has_many :posts
+
+  validates :name, presence: true,  length: { minimum: 1, maximum: 255 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   before_validation :downcase_email
+  has_many :posts
 
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
             format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
